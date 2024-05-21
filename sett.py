32a4e20d26c1e95a7fbb3bc8a663c17cf340025d
ing-jaay <ing.alonsoyanez@gmail.com>
2024-05-20 20:17:30 -0700

    Primer Commit

---
+++ sett.py
@@ -0,0 +1,23 @@
+token = 'bigdateros'
+
+whatsapp_token = 'EAAKPzObkvd4BO9clZABGRok2ODlDkn9d8ZAi2h5jPOa4jCGZAivzGDJLZAvonDbsiTikF9CLORWb3gtcEHtcjtcnAU4H4CdRleWuYe9rOmUH6clpN2rwj4dBE39ozlQnqj3ufurh9WPjkVws6ss3Ar9vZCysDT0zYmdvdfn5kZAGD8CaTlZBSzZCzf98vk2vkTAXEgBD0zoDzAqbfCrO'
+
+whatsapp_url = 'https://graph.facebook.com/v18.0/125349333991570/messages'
+
+stickers = {
+    "poyo_feliz": 984778742532668,
+    "perro_traje": 1009219236749949,
+    "perro_triste": 982264672785815,
+    "pedro_pascal_love": 801721017874258,
+    "pelfet": 3127736384038169,
+    "anotado": 24039533498978939,
+    "gato_festejando": 1736736493414401,
+    "okis": 268811655677102,
+    "cachetada": 275511571531644,
+    "gato_juzgando": 107235069063072,
+    "chicorita": 3431648470417135,
+    "gato_triste": 210492141865964,
+    "gato_cansado": 1021308728970759
+}
+
+document_url = "https://www.africau.edu/images/default/sample.pdf"
